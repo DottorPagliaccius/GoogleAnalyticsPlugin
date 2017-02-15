19af6ffa02747a1a6faae7fff4dded63f2738f3e
mBelliniOD <mbellini@officinedigitali.it>
2017-02-15 17:11:37 +0100

    added nuget support

--- src/GoogleAnalytics/Plugin.GoogleAnalytics.WindowsPhone81/GoogleAnalyticsImplementation.cs
+++ src/GoogleAnalytics/Plugin.GoogleAnalytics.WindowsPhone81/GoogleAnalyticsImplementation.cs
@@ -1,13 +1,37 @@
 using Plugin.GoogleAnalytics.Abstractions;
 using System;
-
+using System.Collections.Generic;
 
 namespace Plugin.GoogleAnalytics
 {
-  /// <summary>
-  /// Implementation for GoogleAnalytics
-  /// </summary>
-  public class GoogleAnalyticsImplementation : IGoogleAnalytics
-  {
-  }
+    /// <summary>
+    /// Implementation for GoogleAnalytics
+    /// </summary>
+    public class GoogleAnalyticsImplementation : IGoogleAnalytics
+    {
+        public void Report(Exception exception, Severity warningLevel = Severity.Warning)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Report(string message, Severity warningLevel = Severity.Warning)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Report(Exception exception, IDictionary<string, string> extraData, Severity warningLevel = Severity.Warning)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TrackEvent(string category, string action, string label, long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TrackPage(string pageName)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
